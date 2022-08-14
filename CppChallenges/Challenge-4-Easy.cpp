ac9c90485baf10143652d0d0fdc9e47f3868b137
Xisurthros <Jacob_Hobbs548@yahoo.com>
2022-08-14 12:43:10 -0500

    Add  CppChallenges/Challenge-4-Easy.cpp  CppChallenges/Challenge-4-Easy.exe

---
+++ CppChallenges/Challenge-4-Easy.cpp
@@ -0,0 +1,46 @@
+#include <cstdlib>
+#include <iostream>
+#include <time.h>
+#include <math.h>
+#include <vector>
+
+std::vector<std::string> passwordGeneration(int numLetters, int numPasswords);
+
+int main(int argc, char *argv[])
+{
+    int numLetters;
+    int numPasswords;
+
+    //get user input
+    std::cout << "Enter number of passwords: ";
+    std::cin >> numPasswords;
+    std::cout << "Enter number of letters: ";
+    std::cin >> numLetters;
+
+    for (int x = 0; x < numPasswords; x++){
+        std::cout << passwordGeneration(numLetters, numPasswords)[x] << std::endl;
+    }
+}
+
+std::vector<std::string> passwordGeneration(int numLetters, int numPasswords)
+{
+    std::string password;
+    std::vector<std::string> passwordList;
+    int i;
+    int j;
+
+    srand(time(NULL));
+
+    //generate passwords
+    for (j = 0; j < numPasswords; ++j){
+        for (i = 0; i < numLetters; ++i){
+            password = password + static_cast<char>((rand() % 93) + 33);
+        };
+        passwordList.push_back(password);
+        password.clear();
+    };
+
+
+    std::cout << "\n\n" << std::endl;
+    return passwordList;
+}
